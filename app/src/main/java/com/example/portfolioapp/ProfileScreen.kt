ab494eb3a2b50c1fc79e91138b54ab6648e537bc
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2022-04-26 15:09:49 +0530

    UI Completed

--- app/src/main/java/com/example/portfolioapp/ProfileScreen.kt
+++ app/src/main/java/com/example/portfolioapp/ProfileScreen.kt
@@ -1,4 +1,4 @@
 package com.example.portfolioapp
 
-class SignUp {
+class ProfileScreen {
 }
--- app/src/main/java/com/example/portfolioapp/SignupScreen.kt
+++ app/src/main/java/com/example/portfolioapp/SignupScreen.kt
@@ -3,7 +3,7 @@ package com.example.portfolioapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_signup : AppCompatActivity() {
+class SignupScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
