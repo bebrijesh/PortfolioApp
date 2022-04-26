dbfcd8ce68bbc42f642d81ab81fe5aae16625113
Brijeshkumar Chavda <chavdabrijeshkumar25@gmail.com>
2022-04-26 13:08:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/portfolioapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.portfolioapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+    }
+}
